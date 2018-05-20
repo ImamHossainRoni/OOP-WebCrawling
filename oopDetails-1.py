704e4a9bcd8d31276017c583dd41e4ff8c4ad306
Imam Hossain Roni <imamhossainroni95@gmail.com>
2018-05-21 02:18:18 +0600

    More details of oop in python

---
+++ oopDetails-1.py
@@ -0,0 +1,15 @@
+class Fighter:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.damage = 10
+
+    def attact(self, other_guy):
+        other_guy.health = other_guy.health - self.damage
+
+roni = Fighter("Roni")
+you = Fighter("Moon")
+print(roni.name)
+print(you.name)
+you.attact(roni)
+print(roni.health)
